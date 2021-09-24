7dad90b3c6cfa2fa0d1f6f2ce1a696df685f7162
Stepan Simonov <90901305+Mrakoboretz@users.noreply.github.com>
2021-09-24 04:20:11 +0300

    Add files via upload

---
+++ dz1.c
@@ -0,0 +1,35 @@
+﻿#include "stdio.h"
+
+int main()
+{
+	int a, i, mass[256];
+	for (i = 0; i < 256; i++)
+	{
+		mass[i] = 0;
+	}
+	printf("insert number: ");
+	scanf_s("%d", &a);
+	i = 0;
+	if (a == 0)
+		printf("0");
+	else {
+		while (a != 0)
+		{
+			if (a % 2 == 0)
+			{
+				mass[i] = 0;
+				a = a / 2;
+			}
+			else
+			{
+				mass[i] = 1;
+				a = a / 2;
+			}
+			i++;
+		}
+		i = i - 1;
+		for (; i >= 0; i--)
+			printf("%d", mass[i]);
+	}
+	return 0;
+}
